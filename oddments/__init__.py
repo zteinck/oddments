d8b391bc37d7bffd05695c599019db67422ace6a
Zachary Einck <zacharyeinck@gmail.com>
2025-05-16 19:50:44 -0500

    update version to 0.3.0

--- oddments/__init__.py
+++ oddments/__init__.py
@@ -1,7 +1,8 @@
 from .iteration import *
 from .pandas import *
 from .sorting import *
+from .text import *
 from .validation import *
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = 'Zachary Einck <zacharyeinck@gmail.com>'
