6144631706a844eab060e624ee6c764099967791
Zachary Einck <zacharyeinck@gmail.com>
2025-11-01 12:41:00 -0500

    refactor: rename select functions and arguments for clarity

--- oddments/iteration/utils.py
+++ oddments/iteration/utils.py
@@ -1,64 +1,98 @@
+from ..validation import validate_value
 
 
-def iter_get(array, index=0, default=None):
-    ''' extends dictionary's .get() to other iterables such as lists '''
-    if array is None:
+def try_get(obj, index=0, default=None):
+    '''
+    Description
+    ------------
+    Like dict.get(), but works for any subscriptable object.
+
+    Parameters
+    ------------
+    obj : any
+        Object from which to get an element.
+    index : int
+        Position of the element as an integer index.
+    default : any
+        Value to return when an IndexError occurs.
+
+    Returns
+    ------------
+    element : any
+        Retrieved element
+    '''
+    validate_value(
+        value=index,
+        name='index',
+        types=int
+        )
+
+    if obj is None:
         return default
+
     try:
-        return array[index]
+        return obj[index]
     except IndexError:
         return default
 
 
-def to_iter(x):
-    ''' converts variable to a list '''
-    return x if isinstance(x, (list, tuple)) else [x]
+def ensure_list(value):
+    ''' ensures a value is a list '''
+    if isinstance(value, (list, tuple)):
+        return list(value)
+    return [value]
 
 
-def delimit_iter(x, func=None, delimiter=', ', quotes='single', encase=True):
+def delimit_iterable(
+    iterable,
+    func=None,
+    delimiter=', ',
+    quotes='single',
+    encase=True
+    ):
     '''
     Description
     ------------
-    Delimits an iterable
+    Delimits an iterable.
 
     Parameters
     ------------
-    x : iterable
-        iterable to delimit
+    iterable : any
+        Iterable to delimit
     func : type | func | None
-        formats each element before it is delimited.
+        Formats each element before it is delimited.
     delimiter : str
-        delimiter
+        String used to separate values.
     quotes : str | None
-        • 'single' ➜ encase each element in single quotes
-        • 'double' ➜ encase each element in double quotes
-        • None ➜ each element is not encased
+        • 'single' ➜ each element is encased in single quotes
+        • 'double' ➜ each element is encased in double quotes
+        • None ➜ No action is taken
     encase : bool
-        if True, the final output is encased in parenthesis
+        If True, the return value is encased in parenthesis.
 
     Returns
     ------------
-    out : str
-        delimited iterable
+    delimited : str
+        Delimited iterable
     '''
     if func is not None:
-        x = list(map(func, x))
+        iterable = list(map(func, iterable))
 
-    x =  [f'{z}' for z in x]
+    iterable =  [f'{x}' for x in iterable]
 
     if quotes is not None:
         q = {'single': "'", 'double': '"'}[quotes]
-        x =  [z.join([q] * 2) for z in x]
+        iterable =  [x.join([q] * 2) for x in iterable]
 
-    x = delimiter.join(x)
-    return f'({x})' if encase else x
+    iterable = delimiter.join(iterable)
+    return f'({iterable})' if encase else iterable
 
 
-def lower_iter(iterable):
+def lower_iterable(iterable):
     return [x.lower() for x in iterable]
 
 
-def text_to_iter(text, transform=str, delimiter='\n'):
+def text_to_iterable(text, transform=str, delimiter='\n'):
     '''
     Description
     ------------
@@ -78,11 +112,15 @@ def text_to_iter(text, transform=str, delimiter='\n'):
     out : list
         list if items in text file
     '''
-    return [transform(x.strip()) for x in text.split(delimiter) if x.strip()]
+    return [
+        transform(x.strip())
+        for x in text.split(delimiter)
+        if x.strip()
+        ]
 
 
 def iter_window(
-    x,
+    iterable,
     left=0,
     right=0,
     strict=False,
@@ -90,12 +128,14 @@ def iter_window(
     include_index=False
     ):
     '''
-    iterates over a window of values in a list
+    Description
+    ------------
+    Iterates over a window of values in a list.
 
     Parameters
     ----------
-    x : list | other iterable
-        list to iterate over
+    iterable : any
+        Object over which to iterate.
     left : int
         how many indices to the left of the current index to include in each
         returned slice
@@ -103,26 +143,26 @@ def iter_window(
         how many indices to the right of the current index to include in each
         returned slice
     strict : bool
-        if True, only complete slices will be returned. (e.g. x=[1, 2, 3, 4, 5]
+        If True, only complete slices will be returned. (e.g. x=[1, 2, 3, 4, 5]
         and left=2 the first slice returned will be [1,2,3] at index 2. Index 0
         and 1 would be partial slices and not returned.
     step : bool
         if True, the right-most index of a slice will define the boundary line
-        for the next slice so that no values repeat. (e.g. x=[1, 2, 3, 4, 5] and
-        right=2 will return slices [1,2,3] and [4,5])
+        for the next slice so that no values repeat. (e.g. x=[1, 2, 3, 4, 5]
+        and right=2 will return slices [1,2,3] and [4,5])
     include_index : bool
         if True, the index of the slice is also returned
     '''
-    n, start = len(x) - 1, -1
+    n, start = len(iterable) - 1, -1
 
-    for i, v in enumerate(x):
+    for i, v in enumerate(iterable):
         a, b = max(0, i - left), min(n, i + right)
 
         if step:
             if left > 0 and i == n: a = max(start, 0)
             if a < start: continue
 
-        subset = x[ a : b + 1 ]
+        subset = iterable[ a : b + 1 ]
         if strict and len(subset) < left + right + 1: continue
         start = b + 1
         yield (i, subset) if include_index else subset
