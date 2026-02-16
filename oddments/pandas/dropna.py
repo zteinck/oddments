179e01ea7b7c2a2662c0adf2b8da661ea76c4739
Zachary Einck <zacharyeinck@gmail.com>
2026-02-16 11:44:40 -0600

    feat: add 'which' parameter to trim_na to specify dropping leading, trailing, or both nulls

--- oddments/pandas/dropna.py
+++ oddments/pandas/dropna.py
@@ -1,48 +1,87 @@
 import numpy as np
 
+from ..validation import validate_value
 from .coercion import with_coerce_series
-from .indexing import verify_index_monotonicity
+from .indexing import verify_index_values
 
 
 @with_coerce_series
-def trim_na(s, inf_as_na=False, raise_on_na=False):
+def trim_na(
+    s,
+    which='both',
+    inf_as_na=False,
+    raise_on_na=False,
+    ):
     '''
     Description
     ------------
-    Drops leading and trailing NaN values from a Series. Only Series with
-    indexes sorted in ascending order are supported.
+    Drops leading and/or trailing null values from a Series.
 
     Parameters
     ------------
+    which : str
+        Specifies which null values to drop:
+            • 'both' → Drop both leading and trailing nulls.
+            • 'leading' → Drop only leading nulls.
+            • 'trailing' → Drop only trailing nulls.
     inf_as_na : bool
         If True, infinite values replaced with NaN.
     raise_on_na : bool
-        If True, an error is raised if interior NaNs are present.
+        If True, an error is raised if any null values remain.
 
     Returns
     ------------
     out : pd.Series
-       Series with leading and trailing NaN values dropped.
+       Series without leading and/or trailing null values.
     '''
 
-    verify_index_monotonicity(s, direction='increasing')
+    validate_value(
+        value=which,
+        name='which',
+        types=str,
+        whitelist=['both','leading','trailing']
+        )
+
+    verify_index_values(s)
+
+    if s.empty:
+        return s
+
+    out = s.copy(deep=True)
 
     if inf_as_na:
-        s.replace(
+        out.replace(
             to_replace=[np.inf, -np.inf],
             value=np.nan,
             inplace=True
             )
 
-    start = s.first_valid_index()
-    stop = s.last_valid_index()
-    out = s.loc[start:stop]
+    if out.isna().all():
+        if raise_on_na:
+            raise ValueError(
+                'Series only contains nulls.'
+                )
+        return out.dropna()
 
-    if not raise_on_na or out.notna().all():
+    first = out.first_valid_index()
+    last = out.last_valid_index()
+
+    bounds = {
+        'both': (first, last),
+        'leading': (first, None),
+        'trailing': (None, last),
+        }
+
+    left, right = bounds[which]
+    out = out.loc[left:right]
+
+    valid = out.notna()
+
+    if not raise_on_na or valid.all():
         return out
 
-    msg = ['Series cannot contain NaN']
-    if inf_as_na: msg.append('or Inf(+/-)')
-    z = s.loc[(out[out.isna()].index)]
-    msg.append(f'between valid values: \n\n{z}\n')
-    raise ValueError(' '.join(msg))
+    nulls = s.loc[(out[(~valid)].index)]
+
+    raise ValueError(
+        f'Series contains nulls:\n\n{nulls}\n'
+        )
