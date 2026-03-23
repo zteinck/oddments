e8085efb8351906973ebc75dfb5e90334723f3ea
Zachary Einck <zacharyeinck@gmail.com>
2026-03-23 17:54:07 -0500

    feat: add support for optional '_repr_attrs' attribute

--- oddments/mixins/repr_mixin.py
+++ oddments/mixins/repr_mixin.py
@@ -7,6 +7,20 @@ class ReprMixin(object):
     #╰-------------------------------------------------------------------------╯
 
     def __repr__(self):
-        content = [f'{k}={v!r},' for k, v in self.__dict__.items()]
+        repr_attrs = getattr(self, '_repr_attrs', None)
+
+        if repr_attrs is None:
+            data = {
+                k: v for k, v in
+                self.__dict__.items()
+                }
+        else:
+            data = {
+                k: getattr(self, k)
+                for k in repr_attrs
+                }
+
+        content = [f'{k}={v!r},' for k, v in data.items()]
         content = '(' + ('\n' + ' ' * 4).join(['', *content, ')'])
-        return self.__class__.__name__ + content
+        text = self.__class__.__name__ + content
+        return text
